350dac85d6a03cd64dde913b699a0b8163a0db78
wicebing <wicebing@gmail.com>
2024-12-20 20:09:33 +0800

    ...

--- composables/useHome.js
+++ composables/useHome.js
@@ -1,3 +1,5 @@
-export const useHome = () => {
-  return {};
+export const useHome = async () => {    
+  const data = await $fetch("https://nuxr3.zeabur.app/api/v1/home/news/")
+    return data
+
 };
--- pages/index.vue
+++ pages/index.vue
@@ -5,6 +5,18 @@ import "vue-loading-overlay/dist/css/index.css";
 const newsList = ref([]);
 const isLoading = ref(false);
 
+// const data = await $fetch("https://nuxr3.zeabur.app/api/v1/home/news/");
+
+// const { data, status } = await useAsyncData('news', async () => {
+//   const [coupons, offers] = await Promise.all([
+//     $fetch("https://nuxr3.zeabur.app/api/v1/home/news/"),
+//     $fetch("https://nuxr3.zeabur.app/api/v1/home/news/")
+//   ])
+
+//   return { coupons, offers }
+// })
+const data = await useHome()
+
 // API 路徑 : https://nuxr3.zeabur.app/api/v1/home/news/
 // 使用 ES6 fetch() 或是 axios.get() 串接 API
 // 切換 isLoading 狀態
@@ -15,8 +27,17 @@ const isLoading = ref(false);
 <template>
   <div class="container">
     <h1>最新消息</h1>
-    <!-- <NewsCard v-for="..." :key="..."  v-bind="..." /> -->
-
+    <NewsCard
+      v-for="item in data.result"
+      :key="item._id"
+      :_id="item._id"
+      :title="item.title"
+      :image="item.image"
+      :description="item.description"
+      :createdAt="item.createdAt"
+      :updatedAt="item.updatedAt"
+    />
+    {{ data }}
     <ClientOnly>
       <Loading v-model:active="isLoading" />
     </ClientOnly>
