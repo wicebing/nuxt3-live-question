ff8f92b21e4901f5d3b9d4a3986eab883d13dbc2
wicebing <wicebing@gmail.com>
2024-12-21 19:24:01 +0800

    ..

--- pages/room.vue
+++ pages/room.vue
@@ -4,29 +4,17 @@ const roomsList = ref([]);
 // apiUrl : https://nuxr3.zeabur.app/api/v1/rooms
 // response 回傳後，將資料寫入 roomsList 變數
 // 使用 roomsList 變數在下方 template 渲染列表
+
+const {data:roomList } = useAsyncData(async () => {
+  const response = await $fetch('https://nuxr3.zeabur.app/api/v1/rooms');
+  return response.result
+});
+
 </script>
 
 <template>
   <h1>房型頁面</h1>
-  <!-- <div class="container mt-4">
-    <div class="row justify-content-center">
-      <div class="col-8 col-md-6 col-lg-3" v-for="...">
-        <div class="card h-100 shadow-sm">
-          <img :src="room.imageUrl" class="card-img-top" alt="Room Image" />
-          <div class="card-body d-flex flex-column">
-            <h3 class="card-title">{{ room.name }}</h3>
-            <p class="card-text flex-grow-1">{{ room.description }}</p>
-            <ul class="list-unstyled">
-              <li><strong>面積:</strong> {{ room.areaInfo }}</li>
-              <li><strong>床型:</strong> {{ room.bedInfo }}</li>
-              <li><strong>最大容納人數:</strong> {{ room.maxPeople }}</li>
-              <li><strong>價格:</strong> {{ room.price }}</li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div> -->
+  <NuxtPage></NuxtPage>
 </template>
 
 <style scoped>
---
+++ pages/room/_id.vue
@@ -0,0 +1,12 @@
+<script setup>
+const route = useRoute();
+const routr = useRouter();
+</script>
+
+<template>
+  <h2>房型詳細頁面 {{ route.fullPath }}</h2>
+
+  <button @click="routr.go(-1)">回上一頁</button>
+</template>
+
+<style scoped></style>
